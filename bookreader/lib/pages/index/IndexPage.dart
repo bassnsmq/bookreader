3ae1ec5d68cf7a433b2cfa955b9c890d3e2d8881
kangkang <2601624788@qq.com>
2019-06-18 09:14:03 +0800

    初始化配置

---
+++ bookreader/lib/pages/index/IndexPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class IndexPage extends StatefulWidget {
+  IndexPage({Key key}) : super(key: key);
+
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
